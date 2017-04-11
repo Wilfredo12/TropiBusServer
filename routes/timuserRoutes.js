1d1b9f139f694d1df8eb97b13766c52e632e6f19
Wilfredo Montanez <Wilfredo Montanez>
2017-04-11 10:41:50 -0400

    finish tim user routes

--- routes/timuserRoutes.js
+++ routes/timuserRoutes.js
@@ -23,22 +23,14 @@ pg.defaults.ssl=true;
 
 //Routes queries declaration 
 
-var getAllRoutes = 'SELECT * FROM route'
-var getRoute = 'SELECT * FROM route NATURAL JOIN routepath WHERE route_id=$1'
-var getAllStops = 'SELECT * FROM stop'  
-var getStopsFromRoute = 'SELECT * FROM stop WHERE route_id=$1'
-var getBusLocation = 'SELECT bus_latitude, bus_longitude FROM Bus NATURAL JOIN GPS WHERE bus_id=$1'
-var getMessages = 'SELECT * FROM message'
-var getBuses = 'SELECT * FROM bus'
-
 var getAllRoutes = 'SELECT * FROM route NATURAL JOIN routepath'
 var getRoute = 'SELECT * FROM route NATURAL JOIN routepath WHERE route_id=$1'
 var getAllStops = 'SELECT * FROM stop'  
 var getStopsFromRoute = 'SELECT * FROM stop NATURAL JOIN route_stop WHERE route_id=$1'
-var getBusLocation = 'SELECT gps_latitude, gps_longitude FROM bus NATURAL JOIN gps NATURAL JOIN route WHERE route_id=$1'
+var getBusLocation = 'SELECT gps_latitude, gps_longitude FROM bus NATURAL JOIN gps WHERE route_id=$1'
 var getMessages = 'SELECT * FROM Message'
 
-var getRouteStatus='SELECT route_id, bus_status from route NATURAL JOIN bus WHERE bus_status="Active" OR bus_status="Changing Turn"'
+var getRouteStatus='SELECT route_id, bus_status from route NATURAL JOIN bus WHERE bus_status<>\'Inactive\''
 
 //get all routes from database and send them back as response
 router.get('/getAllRoutes', function(req, res, next) {
@@ -52,85 +44,92 @@ router.get('/getAllRoutes', function(req, res, next) {
              { console.error(err); res.send("Error " + err); }
             else{
                 //run query
-            // client.query(getRouteStatus, function(err, result1) {
+            client.query(getRouteStatus, function(err, result1) {
 
-            //     if (err)
-            //     { console.error(err); res.send("Error " + err); }
-            //     else{
-            //     var results=result.rows;
-            //     var tempResults=result1.rows;
-            //     //if no buses are active set status of routes to inactive
-            //     if(tempResults.length==0){
-            //         for(var i=0;i<results.length;i++){
-            //             results.bus_status="Inactive"
-            //         }
-            //     }
-            //     //buses are active on some route
-            //     else{
-            //         for(var i=0;i<tempResults.length;i++){
-            //             var active=false;
-            //             for(var j=0;j<results.length;j++){
-            //                 if(results[j].route_id==tempResults[i].route_id){
-            //                     results[j].bus_status=tempResults[i].bus_status
-            //                     active=true;
-            //                     break;
-            //                 }
-            //             }
-            //              if(!active){
-            //                     results[i].bus_status="Inactive"
-            //                 }
-            //         }
-            //     }
-            //     //sending result back as json
-            //     res.json(results.rows);
-            //     done();
-            //     }
-            // });
+                if (err)
+                { console.error(err); res.send("Error " + err); }
+                else{
+                console.log("coji el status de las rutas")
+                var results=result.rows;
+                var tempResults=result1.rows;
+                console.log(tempResults)
+                //if no buses are active set status of routes to inactive
+                if(tempResults.length==0){
+                    for(var i=0;i<results.length;i++){
+                        results.status="Inactive"
+                    }
+                }
+                //some buses are active on some route
+                else{
+                    for(var i=0;i<results.length;i++){
+                        var active=false;
+                        //if bus has an active or changing driver status, set route status to active
+                        for(var j=0;j<tempResults.length;j++){
+                            if(results[i].route_id==tempResults[j].route_id){
+                                results[i].status="Active"
+                                active=true; 
+                                break;                                                     
+                            }
+                        }
+                        //if there is no bus on some route with active or changing driver status,
+                        //assigned route status to inactive
+                         if(!active){
+                                results[i].status="Inactive"
+                            }
+                    } 
+                
+                }
+                //sending result back as json
+                res.json(results);
+                done();
+                }
+                
+            });
             //sending result back as json
-            res.json(result.rows);
-            done();
+            // res.json(result.rows);
+            // done();
             }
         });
     });
 });
 
 //get route with specific route id from database and send the information back as response
-router.get('/getRoute', function(req, res, next) { // Parameter: Route ID
-    console.log("entre a cojer info ruta especifica",req.query.route_id)
-    //connect to database
-    pg.connect(database_URL, function(err, client, done) {
-        //run query
-        client.query(getRoute, [req.query.route_id], function(err, result) {
+// router.get('/getRoute', function(req, res, next) { // Parameter: Route ID
+//     console.log("entre a cojer info ruta especifica",req.query.route_id)
+//     //connect to database
+//     pg.connect(database_URL, function(err, client, done) {
+//         //run query
+//         client.query(getRoute, [req.query.route_id], function(err, result) {
 
-            if (err)
-             { console.error(err); res.send("Error " + err); }
-            else{
-            //sending result back as json
-            res.json(result.rows);
-            done();
-            }
-        });
-    });
-});
+//             if (err)
+//              { console.error(err); res.send("Error " + err); }
+//             else{
+//             //sending result back as json
+//             res.json(result.rows);
+//             done();
+//             }
+//         });
+//     });
+// });
 
 //get all bus stops information from database and send the information back as response
-router.get('/getAllStops', function(req, res, next) {
-    console.log("entre cojer info de todas las paradas")
-    //connecting to database
-    pg.connect(database_URL, function(err, client, done) {
-        //run query
-        client.query(getAllStops, function(err, result) {
+// router.get('/getAllStops', function(req, res, next) {
+//     console.log("entre cojer info de todas las paradas")
+//     //connecting to database
+//     pg.connect(database_URL, function(err, client, done) {
+//         //run query
+//         client.query(getAllStops, function(err, result) {
 
-            if (err)
-             { console.error(err); res.send("Error " + err); }
-            else{
-            //sending result back as json
-            res.json(result.rows);
-            done();
-            }
-        });
-    });
-});
+//             if (err)
+//              { console.error(err); res.send("Error " + err); }
+//             else{
+//             //sending result back as json
+//             res.json(result.rows);
+//             done();
+//             }
+//         });
+//     });
+// });
 
 //get all bus stops from specific route and send info back as response
 router.get('/getStopsFromRoute', function(req, res, next) {//Parameter: Route ID
@@ -189,7 +188,82 @@ router.get('/getMessages', function(req, res, next) {
     });
 });
 
+//script to populate stop table in database
+// router.get('/setUpStops', function(req, res, next) {
+//     console.log("entre cojer info de todas las paradas")
+//     //connecting to database
+//     pg.connect(database_URL, function(err, client, done) {
+//         //run query
+//         for(var i=0;i<stops.length;i++){
+            
+//             var stop=stops[i];
+//             console.log(stop)
+//             var query="INSERT INTO stop(stop_name,stop_description,stop_latitude,stop_longitude) VALUES (\'"+stop.stop_name+"\',\'"+stop.stop_description+"\',"+stop.stop_latitude+","+stop.stop_longitude+")";
+//             client.query(query, function(err, result) {
 
+//             if (err)
+//              { console.error(err); res.send("Error " + err); }
+//             else{
+//             //sending result back as json
+            
+//             }
+//         });
+//         }
+//         res.json({success:1});
+//         done();
+//     });
+// });
+
+// router.get('/setUpRoutes', function(req, res, next) {
+//     console.log("entre a insertar rutas")
+//     //connecting to database
+//     pg.connect(database_URL, function(err, client, done) {
+//         //run query
+//         for(var i=0;i<routes.length;i++){
+            
+//             var route=routes[i];
+//             console.log(route)
+//             var query="INSERT INTO route(route_name,route_description,route_area) VALUES (\'"+route.route_name+"\',\'"+route.route_description+"\',\'"+route.route_area+"\')";
+//             client.query(query, function(err, result) {
+
+//             if (err)
+//              { console.error(err); res.send("Error " + err); }
+//             else{
+//             //sending result back as json
+            
+//             }
+//         });
+//         }
+//         res.json({success:1});
+//         done();
+//     });
+// });
+
+// router.get('/setUpRoutePaths', function(req, res, next) {
+//     console.log("entre a insertar path de las rutas")
+//     //connecting to database
+//     pg.connect(database_URL, function(err, client, done) {
+//         //run query
+        
+//         for(var i=0;i<routes.length;i++){
+            
+//             var route=routes[i];
+//             console.log(route)
+//             var query="INSERT INTO routepath(route_path) VALUES (\'"+route.path+"\')";
+//             client.query(query, function(err, result) {
+
+//             if (err)
+//              { console.error(err); res.send("Error " + err); }
+//             else{
+//             //sending result back as json
+            
+//             }
+//         });
+//         }
+//         res.json({success:1});
+//         done();
+//     });
+// });
 
 module.exports=router;
 
